2c970663ae7c5be5d84d15cceb2bd821d2ab6d5b
CA01971028 <CA01971028@st.kawahara.ac.jp>
2025-05-07 10:24:09 +0900

    背景色を追加
    背景色とフォームの枠を作成

---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+
+const Form = () => {
+    return (
+        <>
+            <div className="w-[60%] h-[80%] flex items-center justify-center rounded-lg bg-white">
+
+            </div>
+        </>
+    );
+}
+
+export default Form;
