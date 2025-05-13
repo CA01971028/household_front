4b49803369ca2072fcd94cf795c3e44a8250f6a9
CA01971028 <CA01971028@st.kawahara.ac.jp>
2025-05-13 09:44:08 +0900

    収入と支出の背景を追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,12 @@ import BottonIcon from "@/components/BottonIcon";
 const page = () => {
     return (
         <div className="h-svh w-screen bg-green-300">
+            <div className="flex flex-col">
+                <div className="flex flex-row justify-around">
+                    <div className="w-[48vw] h-[12svh] rounded-md" style={{backgroundColor:'rgba(245, 245, 245, 1)'}}>Income</div>
+                    <div className="w-[48vw] rounded-md" style={{backgroundColor:'rgba(245, 245, 245, 1)'}}>Expenses</div>
+                </div>
+            </div>
             <div className="absolute w-screen bottom-0 h-[15svh]" style={{ backgroundColor: 'rgba(239, 239, 239, 1)' }}>
                 <BottonIcon/>
             </div>
--- src/components/BottonIcon.tsx
+++ src/components/BottonIcon.tsx
@@ -1,28 +1,28 @@
 import HomeIcon from '@mui/icons-material/Home';
 import SavingsIcon from '@mui/icons-material/Savings';
-import AccountBalanceWallet from '@mui/icons-material';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 const BottonIcon = () => {
     return (
-        <div className='flex flex-row items-center justify-center h-[15svh]'>
-            <div className="flex flex-col">
-                <button className="my-auto rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
-                    <SavingsIcon className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
-                </button>
-                <div className='text-lg'>予算</div>
-            </div>
-            <div className="flex flex-col">
-                <button className="my-auto rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
-                    <HomeIcon className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
-                </button>
-                <div className='text-lg'>ホーム</div>
-            </div>
-            <div className="flex flex-col">
-                <button className="my-auto rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
-                    <AccountBalanceWallet className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
-                </button>
-                <div className='text-lg'>支出/収入</div>
-            </div>
-        </div>
+                <div className="flex flex-row justify-around items-center h-[15svh]">
+                    <div className="flex flex-col items-center">
+                        <button className="rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
+                        <SavingsIcon className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
+                        </button>
+                        <div className="text-lg mt-1">予算</div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <button className="rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
+                        <HomeIcon className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
+                        </button>
+                        <div className="text-lg mt-1">ホーム</div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <button className="rounded-full bg-white shadow-md hover:bg-green-100 transition-colors duration-200 size-[3.4rem]">
+                        <AccountBalanceWalletIcon className="text-green-600 hover:text-green-800 transition-colors duration-200" fontSize="large" />
+                        </button>
+                        <div className="text-lg mt-1">支出/収入</div>
+                    </div>
+                </div>
     );
 }
 
