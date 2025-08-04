63c668bd214b3200f7576b1f3c3873e8c59f49ba
CA01971028 <CA01971028@st.kawahara.ac.jp>
2025-08-04 12:13:01 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import Form from "../components/Form";
-import { useAuthCheck } from "../app/hooks/useAuthCheck"
+// import { useAuthCheck } from "../app/hooks/useAuthCheck"
 
 export default function Home() {
-  const { loading } = useAuthCheck({
-    requireAuth: false,
-    redirectTo: '/home',
-  });
+  // const { loading } = useAuthCheck({
+  //   requireAuth: false,
+  //   redirectTo: '/home',
+  // });
 
-  if (loading) return null; // チェック中は何も表示しない（もしくはローディングUIに）
+  // if (loading) return null; // チェック中は何も表示しない（もしくはローディングUIに）
 
   return (
     <div className="h-svh w-screen bg-green-300 flex items-center justify-center">
